1eea94c91de9cf0a6d078f55b61faa56a9b6fb88
François Patron <francois.patron@kandy.eu>
2024-08-26 11:32:15 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,185 @@
+# The Innovation Game Dashboard
+
+This project allows you to monitor your TIG nodes directly in Grafana, providing the necessary information and facilitating tracking.
+
+<img src="dashboard.png" />
+
+## Prerequisites
+
+It is preferable to have your own Grafana server for monitoring your nodes. But you can use Grafana Cloud which will provide you with the necessary stack [https://grafana.com/](https://grafana.com/)
+
+To make the dashboard work, you need:
+* Grafana (dashboard)
+* Victoria Metrics (metrics service)
+
+## Quick start
+
+If you don't want manually install this project, follow these instructions:
+
+1. Install grafana server: follow instructions [here](#install_grafana)
+2. Install alloy agent on server up 24/24 **<span style="color:red">(this only works on Ubuntu 22.04 and Debian 12)</span>** 
+```
+bash <(wget -qO- https://raw.githubusercontent.com/fpatron/tig-dashboard/master/exporter/install_linux.sh)
+```
+3. Import grafana Dashboard: follow instructions [here](#import_dashboard)
+
+## Installing Grafana / Victoria Metrics / Loki <a id='install_grafana'></a>
+
+You have multiple choices to create your Grafana instance:
+
+* Use Grafana Cloud: they provided a limited free plan
+* Use the provided Docker image to create your own Grafana instance
+1. Install Docker
+```
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+```
+2. Install Docker image
+```
+cd
+mkdir grafana
+cd grafana
+wget https://raw.githubusercontent.com/fpatron/tig-dashboard/master/docker/docker-compose.yml
+sudo docker compose up -d
+```
+* Create your own Grafana instance on a VM or server of your choice
+
+### Installing the TIG data exporter
+
+It is necessary to install the custom exporter in order to send custom metrics to grafana:
+
+* Create a directory `tig_exporter` on your server
+ ```
+ mkdir -p ~/tig_exporter
+ ```
+* Get files [tig_exporter.py](exporter/tig_exporter.py) [requirements.txt](exporter/requirements.txt) into
+```
+cd ~/tig_exporter
+wget https://raw.githubusercontent.com/fpatron/tig-dashboard/master/exporter/tig_exporter.py
+wget https://raw.githubusercontent.com/fpatron/tig-dashboard/master/exporter/requirements.txt
+```
+* Setup exporter script
+Replace the variables ```PLAYER_IDS``` and ```INNOVATOR_IDS ```
+Example:
+```
+PLAYER_IDS = ["0x73C7A2e1C9eb014EDbad892487D4cd4FEc5B239f","0x73C7A2e1C9eb014EDbad892487D4cd4FEc5B239f"]
+INNOVATOR_IDS = []
+``` 
+* Prepare python environment
+```
+sudo apt install python3 python3-pip python3-virtualenv
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+* Create a dedicated service to launch the exporter at runtime (see file [tig_exporter.service](exporter/tig_exporter.service))
+```
+sudo nano /lib/systemd/system/tig_exporter.service
+```
+
+```
+[Unit]
+Description=TIG Exporter Service
+After=network.target
+
+[Service]
+User=<your_username>
+Group=<your_username>
+WorkingDirectory=/home/<your_username>/tig_exporter
+ExecStart=/home/<your_username>/tig_exporter/venv/bin/python /home/<your_username>/tig_exporter/tig_exporter.py
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+```
+Replace the <username> values and paths if needed with the correct values.
+
+* Enable the service:
+```
+sudo systemctl daemon-reload
+sudo systemctl enable tig_exporter
+sudo systemctl start tig_exporter
+```
+
+### Installing Grafana Alloy
+
+It is necessary to install the Grafana Alloy agent on your server. It will be responsible for reporting your TIG metrics.
+
+To install it, run these commands:
+```
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null                              
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
+sudo apt update
+sudo apt-get install alloy -y
+
+```
+
+### Configuring Grafana Alloy
+
+Once the agent is installed, you need to configure it.
+The configuration file is located here:
+```
+/etc/alloy/config.alloy
+```
+Delete its contents and replace them with the contents of the example file [alloy/config.alloy](alloy/config.alloy)
+
+Replace the following tags with your own information:
+
+* <PROMETHEUS_ENDPOINT> (eg: http://X.X.X.X:9090/api/v1/write)
+* <PROMETHEUS_USERNAME> (optional)
+* <PROMETHEUS_PASSWORD> (optional)
+
+If you use the Grafana Cloud suite, you will need to generate an API key for Prometheus and Loki.
+Replace the Prometheus password with this key.
+
+Restart alloy 
+
+```
+sudo systemctl restart alloy
+```
+
+
+## Importing the Dashboard <a id='import_dashboard'></a>
+
+Go to your Grafana instance
+
+
+1. Install the Infinity plugin
+    * Go to Home > Administration > Plugins and data > Plugins
+    * Note: By default, only installed plugins are displayed. Switch to "All" mode (top right).
+    * Search for the Infinity plugin and install it
+
+2. Add a datasource for Infinity, Loki and Prometheus
+    * Go to Home > Data sources
+    * Click on the "Add new data sources" button
+    * Search for "Infinity"
+    * Click on "Save & test"
+    * Click on the "Add new data sources" button
+    * Search for "Prometheus"
+    * Connection : Prometheus URL : http://192.168.X.X:9090    (put the IP of the computer running docker)
+    * Click on "Save & test"
+
+3. Import the dashboard
+    * Go to Home > Dashboard
+    * Download the dashboard in [JSON format](https://raw.githubusercontent.com/fpatron/tig-dashboard/master/grafana/The%20Innovation%20Game%20-%20Dashboard.json)
+    * Click on the "New" > "Import" button
+    * Upload the dashboard in JSON format
+    * Select the various datasources
+
+
+Once everything is set up, wait at least 30 minutes to get good metrics on your dashboard :)
+
+That's all! Enjoy!
---
+++ exporter/tig_exporter.py
@@ -0,0 +1,166 @@
+from flask import Flask, Response
+from prometheus_client import Gauge, CollectorRegistry, generate_latest, start_http_server
+import requests
+from datetime import timedelta
+import logging
+
+app = Flask(__name__)
+
+# TO BE UPDATED WITH YOUR ADDRESSES
+PLAYER_IDS = []
+INNOVATOR_IDS = []
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+registry = CollectorRegistry()
+block_metric = Gauge('tig_block_info', 'Block information', ['round', 'metric'], registry=registry)
+player_metric = Gauge('tig_player_data', 'Player data per block', ['round', 'player_id', 'metric'], registry=registry)
+innovator_metric = Gauge('tig_innovator_data', 'Innovator data per block', ['round', 'player_id', 'metric'], registry=registry)
+challenge_metric = Gauge('tig_challenge_data', 'Challenge data per player', ['round', 'player_id', 'metric', 'challenge_id', 'challenge_name'], registry=registry)
+algorithm_metric = Gauge('tig_algorithm_data', 'Algorithm data per player', ['round', 'player_id', 'metric', 'algorithm_id', 'algorithm_name'], registry=registry)
+
+def get_json_response(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Request error: {e}")
+        return None
+
+def get_last_block():
+    url = "https://mainnet-api.tig.foundation/get-block"
+    return get_json_response(url)
+
+def get_price_data():
+    url = "https://api.dexscreener.com/latest/dex/pairs/base/0x5280d5e63b416277d0f81fae54bb1e0444cabdaa"
+    return get_json_response(url)
+
+def get_players(block_id, player_type="benchmarker"):
+    url = f"https://mainnet-api.tig.foundation/get-players?block_id={block_id}&player_type={player_type}"
+    data = get_json_response(url)
+    return data.get('players', []) if data else []
+
+def get_algorithms(block_id):
+    url = f"https://mainnet-api.tig.foundation/get-algorithms?block_id={block_id}"
+    data = get_json_response(url)
+    return data.get('algorithms', []) if data else []
+
+def get_challenges(block_id):
+    url = f"https://mainnet-api.tig.foundation/get-challenges?block_id={block_id}"
+    data = get_json_response(url)
+    challenges = {}
+    if data and "challenges" in data:
+        for challenge in data["challenges"]:
+            challenges[challenge["id"]] = challenge["details"].get("name", "unknown")
+    return challenges
+
+def filter_and_record_metrics(last_block, price_data, players_data, innovators_data, algorithms_data, challenges):
+    player_ids = {player_id.lower(): player_id for player_id in PLAYER_IDS}
+    innovator_ids = {innovator_id.lower(): innovator_id for innovator_id in INNOVATOR_IDS}
+
+    round_number = last_block.get('block', {}).get('details', {}).get('round', 0)
+    latest_block_height = int(last_block.get('block', {}).get('details', {}).get('height', 0))
+    blocks_per_round = int(last_block.get('block', {}).get('config', {}).get('rounds', {}).get('blocks_per_round', 0))
+
+    round_start_block = (latest_block_height // blocks_per_round) * blocks_per_round
+    round_end_block = round_start_block + blocks_per_round - 1
+    round_blocks_left = round_end_block - latest_block_height
+
+    block_metric.labels(round=round_number, metric='priceUsd').set(price_data.get('pair', {}).get('priceUsd', 0))
+    block_metric.labels(round=round_number, metric='priceVolume').set(price_data.get('pair', {}).get('volume', {}).get('h24', 0))
+    block_metric.labels(round=round_number, metric='priceChange').set(price_data.get('pair', {}).get('priceChange', {}).get('h24', 0))
+    block_metric.labels(round=round_number, metric='priceLiquidity').set(float(price_data.get('pair', {}).get('liquidity', {}).get('usd', 0)))
+        
+    block_metric.labels(round=round_number, metric='height').set(latest_block_height)
+    block_metric.labels(round=round_number, metric='round').set(round_number)
+    block_metric.labels(round=round_number, metric='blocks_per_round').set(blocks_per_round)
+    block_metric.labels(round=round_number, metric='round_start_block').set(round_start_block)
+    block_metric.labels(round=round_number, metric='round_end_block').set(round_end_block)
+    block_metric.labels(round=round_number, metric='round_blocks_left').set(round_blocks_left)
+    block_metric.labels(round=round_number, metric='rewards').set(last_block.get('block', {}).get('config', {}).get('rewards', {}).get('schedule', {})[1].get('block_reward', 0))
+
+    for player in players_data:
+        player_id = player.get('id', '').lower()
+        if player_id in player_ids:
+            block_data = player.get('block_data', {})
+
+            for key, value in block_data.items():
+                if key == 'num_qualifiers_by_challenge':
+                    for challenge_id, num_qualifiers in value.items():
+                        challenge_name = challenges.get(challenge_id, "unknown")
+                        challenge_metric.labels(
+                            round=round_number,
+                            player_id=player_id,
+                            metric='num_qualifiers_by_challenge',
+                            challenge_id=challenge_id, 
+                            challenge_name=challenge_name
+                        ).set(num_qualifiers)
+                elif key == 'cutoff':
+                    player_metric.labels(round=round_number, player_id=player_id, metric=key).set(value)
+                else:
+                    converted_value = 0
+                    if isinstance(value, str):
+                        try:
+                            converted_value = float(value) / 1e18
+                        except ValueError:
+                            converted_value = 0
+                    player_metric.labels(round=round_number, player_id=player_id, metric=key).set(converted_value)
+    
+    for innovator in innovators_data:
+        innovator_id = innovator.get('id', '').lower()
+        if innovator_id in innovator_ids:
+            block_data = innovator.get('block_data', {})
+
+            for key, value in block_data.items():
+                converted_value = 0
+                if isinstance(value, str):
+                    try:
+                        converted_value = float(value) / 1e18
+                    except ValueError:
+                        converted_value = 0
+                logger.info(f"Add for player {innovator_id} key {key} value {converted_value}")
+                innovator_metric.labels(round=round_number, player_id=innovator_id, metric=key).set(converted_value)
+
+    for algo in algorithms_data:
+        algo_id = algo.get('id')
+        algo_name = algo.get('details', {}).get('name')
+        block_data = algo.get('block_data', {})
+
+        if block_data:
+            num_qualifiers_by_player = block_data.get('num_qualifiers_by_player', {})
+            for player_id, num_qualifiers in num_qualifiers_by_player.items():
+                if player_id in player_ids:
+                    algorithm_metric.labels(round=round_number, player_id=player_id, metric='num_qualifiers_by_algo', algorithm_id=algo_id, algorithm_name=algo_name).set(num_qualifiers)
+
+@app.route('/metrics')
+def metrics():
+    last_block = get_last_block()
+    if last_block:
+        last_block_id = last_block.get('block', {}).get('id')
+        if last_block_id:
+            price_data = get_price_data()
+            if price_data:
+                players_data = get_players(last_block_id)
+                innovators_data = get_players(last_block_id, player_type="innovator")
+                algorithms_data = get_algorithms(last_block_id)
+                challenges = get_challenges(last_block_id)
+                
+                if players_data and algorithms_data and challenges:
+                    filter_and_record_metrics(last_block, price_data, players_data, innovators_data, algorithms_data, challenges)
+                else:
+                    logger.error("Failed to retrieve players, algorithms, or challenges data")
+            else:
+                logger.error("Failed to retrieve price data")        
+        else:
+            logger.error("Failed to retrieve the last block id")
+    else:
+        logger.error("Failed to retrieve the last block")
+
+    return Response(generate_latest(registry), mimetype='text/plain')
+
+if __name__ == '__main__':
+    start_http_server(8001)
+    app.run(host='127.0.0.1', port=5002)
