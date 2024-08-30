187fcb1a5bdc3778ec2a379ddc99680db399f6ab
François Patron <francois.patron@kandy.eu>
2024-08-30 16:04:53 +0200

    Fix scripts

--- docker/install_allinone.sh
+++ docker/install_allinone.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-mkdir -p alloy && mkdir -p exporter 
+mkdir -p alloy && mkdir -p exporter && mkdir -p grafana/provisioning/datasources
 wget https://raw.githubusercontent.com/fpatron/tig-dashboard/master/docker/docker-compose-allinone.yml
 wget https://raw.githubusercontent.com/fpatron/tig-dashboard/master/docker/docker-compose-grafana.yml
 wget https://raw.githubusercontent.com/fpatron/tig-dashboard/master/docker/docker-compose-exporter.yml
 wget https://raw.githubusercontent.com/fpatron/tig-dashboard/master/docker/alloy/Dockerfile -P alloy
 wget https://raw.githubusercontent.com/fpatron/tig-dashboard/master/docker/exporter/Dockerfile -P exporter
+wget https://raw.githubusercontent.com/fpatron/tig-dashboard/master/docker/grafana/provisioning/datasources/datasources.yml  -P grafana/provisioning/datasources
--- docker/install_exporter.sh
+++ docker/install_exporter.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-mkdir -p alloy && mkdir -p exporter 
+mkdir -p alloy && mkdir -p exporter && mkdir -p grafana/provisioning/datasources
 wget https://raw.githubusercontent.com/fpatron/tig-dashboard/master/docker/docker-compose-exporter.yml
 wget https://raw.githubusercontent.com/fpatron/tig-dashboard/master/docker/alloy/Dockerfile -P alloy
 wget https://raw.githubusercontent.com/fpatron/tig-dashboard/master/docker/exporter/Dockerfile -P exporter
+wget https://raw.githubusercontent.com/fpatron/tig-dashboard/master/docker/grafana/provisioning/datasources/datasources.yml  -P grafana/provisioning/datasources
